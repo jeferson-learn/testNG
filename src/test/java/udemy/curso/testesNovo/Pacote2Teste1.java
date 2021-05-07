ee33e123caa9b20c614e053deee14e7c2a555037
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2021-05-07 18:37:47 -0300

    Coloquei anotacao grupo cadas metodos

--- src/test/java/udemy/curso/testesNovo/Pacote2Teste1.java
+++ src/test/java/udemy/curso/testesNovo/Pacote2Teste1.java
@@ -2,22 +2,23 @@ package udemy.curso.testesNovo;
 
 import org.testng.annotations.Test;
 
+@Test(groups = "teste-ordem")
 public class Pacote2Teste1 {
 
-    @Test
+    @Test(groups = "integracao")
     public void pacote2Teste1() {
-        System.out.println("Primeira teste da classe 1 do pacote 2");
+        System.out.println("Primeira teste da classe 1 do pacote 2 I");
     }
-    @Test
+    @Test(groups = "unitario")
     public void pacote2Teste2() {
-        System.out.println("Segunda teste da classe 1 do pacote 2");
+        System.out.println("Segunda teste da classe 1 do pacote 2 U");
     }
-    @Test
+    @Test(groups = "integracao")
     public void pacote2Teste3() {
-        System.out.println("Terceira teste da classe 1 do pacote 2");
+        System.out.println("Terceira teste da classe 1 do pacote 2 I");
     }
-    @Test
+    @Test(groups = "unitario")
     public void pacote2Teste4() {
-        System.out.println("Quarta teste da classe 1 do pacote 2");
+        System.out.println("Quarta teste da classe 1 do pacote 2 U");
     }
 }
