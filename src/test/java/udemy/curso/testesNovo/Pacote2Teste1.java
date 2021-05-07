58641dedd82215cea9b9ca27fb21b4091527cf6d
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2021-05-07 16:55:29 -0300

    Criei classe outro pacote e classe, para teste

---
+++ src/test/java/udemy/curso/testesNovo/Pacote2Teste1.java
@@ -0,0 +1,23 @@
+package udemy.curso.testesNovo;
+
+import org.testng.annotations.Test;
+
+public class Pacote2Teste1 {
+
+    @Test
+    public void pacote2Teste1() {
+        System.out.println("Primeira teste da classe 1 do pacote 2");
+    }
+    @Test
+    public void pacote2Teste2() {
+        System.out.println("Segunda teste da classe 1 do pacote 2");
+    }
+    @Test
+    public void pacote2Teste3() {
+        System.out.println("Terceira teste da classe 1 do pacote 2");
+    }
+    @Test
+    public void pacote2Teste4() {
+        System.out.println("Quarta teste da classe 1 do pacote 2");
+    }
+}
