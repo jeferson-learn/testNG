4cf388c6eb0f8ead580bceb7b0e8e26b9f7705cd
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2021-05-10 18:12:11 -0300

    Coloquei SkipException e ErrorsException

--- src/test/java/udemy/curso/testes/TestClasse.java
+++ src/test/java/udemy/curso/testes/TestClasse.java
@@ -1,5 +1,7 @@
 package udemy.curso.testes;
 
+import com.google.inject.internal.ErrorsException;
+import org.testng.SkipException;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -14,23 +16,29 @@ public class TestClasse {
     @Test(groups = "unitario")
     public void teste1Classe2() {
         System.out.println("Primeira teste da classe 2 U");
+        throw new SkipException("O teste foi Skipped");
     }
+
     @Test(groups = "unitario")
     public void teste2Classe2() {
         System.out.println("Segunda teste da classe 2 U");
     }
+
     @Test(groups = "integracao")
-    public void teste3Classe2() {
+    public void teste3Classe2() throws ErrorsException {
         System.out.println("Terceira teste da classe 2 I");
+        throw new ErrorsException(null);
     }
     @Test(groups = "integracao")
     public void teste4Classe2() {
         System.out.println("Quarta teste da classe 2 I");
     }
+
     @Test(groups = "impar")
     public void teste5Classe2() {
         System.out.println("Quinta teste da classe 2");
     }
+
     @Test(groups = "par")
     public void teste6Classe2() {
         System.out.println("Sexta teste da classe 2");
